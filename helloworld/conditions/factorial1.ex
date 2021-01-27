ea65967bb113d9dc0b9a6bf466f75ad5544e117c
juan carlos meza <juanm0507@gmail.com>
2021-01-27 14:34:32 -0500

    factorial actualizacion

--- helloworld/conditions/factorial1.ex
+++ helloworld/conditions/factorial1.ex
@@ -1,12 +1,12 @@
 defmodule Factorial do
-  def classic  when is_integer(n) and n >= 0 do
+  def classic(n)  when is_integer(n) and n >= 0 do
     cond do
       n < 2 -> 1
       true -> facp1(n, 0, 1)
     end
   end
 
-def fac1(n) when is_integer(n) and n>0 do
+def fac1(n) when is_integer(n) and n >= 0 do
   facp1(n, 0, 1)
 end
 
@@ -27,7 +27,37 @@ def facp1(n, i, acc) when is_integer(n) do
     true -> facp1(n, i + 1,acc * (i + 1))
   end
 end
+# fac2(n) = acc(n, k * fac2(n -k)
+# acc(n, k) = n *(n-1) * ....*(n - k + 1)
+# acc(n, k) = acc(n, k-1) * (n - k + 1)
 
+  def facp2(0, acc) when is_integer(acc) do
+     acc
+  end
+
+  def fac2(n) when is_integer(n) and n >= 0 do
+    cond do
+      n < 1 -> 1
+      true -> facp2(n - 1,n)
+    end
+  end
+
+
+  def facp2(n, acc) when is_integer(n) do
+    facp2(n - 1 , acc * n)
+  end
 end
 
-#IO.puts (Factorial.classic(1,5,"for uno"))
+#IO.puts (Factorial.fact1(1))
+IO.puts (Factorial.classic(6))
+IO.puts ("fac1(0) = #{Factorial.fac1(0)}")
+IO.puts ("fac1(1) = #{Factorial.fac1(1)}")
+IO.puts ("fac1(2) = #{Factorial.fac1(2)}")
+IO.puts ("fac1(3) = #{Factorial.fac1(3)}")
+IO.puts ("fac1(4) = #{Factorial.fac1(4)}")
+IO.puts ("fac1(5) = #{Factorial.fac1(5)}")
+IO.puts("=========================")
+
+IO.puts ("fac2(3) = #{Factorial.fac2(3)}")
+IO.puts ("fac2(4) = #{Factorial.fac2(4)}")
+IO.puts ("fac2(5) = #{Factorial.fac2(5)}")
