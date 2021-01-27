b99d22223fa8777c7b10307584e8dfff7ad4714a
juan carlos meza <juanm0507@gmail.com>
2021-01-27 16:51:39 -0500

    calculo collatz

---
+++ helloworld/conditions/collatz.ex
@@ -0,0 +1,15 @@
+defmodule Collatz do
+  def for_collatz(n) do
+    IO.puts("#{n}")
+    if n > 1 do
+      valor = if rem(n, 2) == 0 do
+         div(n, 2)
+      else
+         (n * 3) + 1
+      end
+      for_collatz(valor)
+    end
+  end
+end
+
+Collatz.for_collatz(23)
