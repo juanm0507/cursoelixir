f998805e8662fd3359f9c0ef91177b88fc9d2830
juan carlos meza <juanm0507@gmail.com>
2021-02-01 16:56:18 -0500

    exercise5

---
+++ helloworld/examen1/exercise5.ex
@@ -0,0 +1,5 @@
+#Respuesta  a la pregunta 5.
+
+# Si claro se pude implementar,  por que se  utiliza variables acumuladoras en caso de
+# reduce la cual va guardando el resultado. y con map también funciona utlizar
+# con la estructura clave y valor.
