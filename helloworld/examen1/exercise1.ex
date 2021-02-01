0bf638a1a68df3bf566698ccc2a0d60e3f2a2ac7
juan carlos meza <juanm0507@gmail.com>
2021-02-01 16:51:48 -0500

    exercise1

---
+++ helloworld/examen1/exercise1.ex
@@ -0,0 +1,18 @@
+defmodule SumAccumulate do
+
+  def sum(list) when is_list(list) do
+    listVacia = []
+    IO.inspect("#{sump(0,0,list,listVacia)}")
+  end
+
+  defp sump(i,suma,list,listr) do
+    cond do
+      length(list) == 0 -> 0
+      i < length(list) -> sump(i + 1,suma + Enum.at(list,i),list,listr ++ [suma])
+      true ->  listr
+    end
+  end
+end
+
+SumAccumulate.sum([])
+SumAccumulate.sum([2, 5, 20])
