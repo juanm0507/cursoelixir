405a89f7e7da599d31e13c330ef2a8413003c1f4
juan carlos meza <juanm0507@gmail.com>
2021-02-03 17:12:24 -0500

    funcion is_zero

---
+++ helloworld/conditions/is_zero.ex
@@ -0,0 +1,18 @@
+defmodule Zero do
+  #def is_zero(n) when  is_integer(n) do
+   #  n  == 0
+  #end
+
+
+  def is_zero(0) do
+    true
+  end
+
+  def is_zero(n) when  is_integer(n) do
+    false
+  end
+
+end
+IO.puts (Zero.is_zero(0))
+IO.puts (Zero.is_zero(1))
+# IO.puts (Zero.is_zero(1.5)) # throws  a FunctionClauseError
